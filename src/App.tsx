64f0ab9d02e796fe91f534b5c4ef407d1f9134c7
du770 <eduarda.s.lopess@hotmail.com>
2025-07-30 10:11:13 -0300

    criada classe usuario

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Usuario } from './models/Usuario.ts'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const u1 = new Usuario('Duda', 18);
 
   return (
     <>
-      
+      {u1.apresentar()}
     </>
   )
 }
